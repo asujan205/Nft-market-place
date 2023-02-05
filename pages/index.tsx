c489d161d441908dd41c3fecfa10c3eae2e068e1
Sujan Adhikari <Asujan205@gmail.com>
2023-02-05 16:49:00 +0545

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 import CreationForm from "../components/CreationNft/creation";
 import Homepage from "../components/Homepage/Homepage";
-import FetchUserNft from "../components/FetchMynfts";
+import FetchUserNft from "../components/FetchMynfts/fetchBuyed";
 import FetchListed from "../components/FetchMynfts/fetchListed";
 
 export default function Home() {
