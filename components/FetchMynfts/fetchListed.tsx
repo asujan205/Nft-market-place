6659fe15951ebd3c7be93a0cd351688386c5e022
Sujan Adhikari <Asujan205@gmail.com>
2023-02-05 16:01:07 +0545

    fetchlistyed

--- components/FetchMynfts/fetchListed.tsx
+++ components/FetchMynfts/fetchListed.tsx
@@ -6,7 +6,7 @@ import NftCard from "../MiniComponents/NftCard";
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contractAddress = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 const contract = new web3.eth.Contract(NftAbi, contractAddress);
-const FetchBuyed = () => {
+const FetchListed = () => {
   const [nfts, setMynfts] = useState<any[]>([]);
   const fetchNfts = async () => {
     const accounts = await web3.eth.getAccounts();
@@ -67,3 +67,4 @@ const FetchBuyed = () => {
     </>
   );
 };
+export default FetchListed;
