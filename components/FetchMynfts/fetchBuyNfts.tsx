0c5c4b23d20b484f61b9038e167175f984087b48
Sujan Adhikari <Asujan205@gmail.com>
2023-02-05 15:46:06 +0545

    FetchingMyBuyedItesm

--- components/FetchMynfts/fetchBuyNfts.tsx
+++ components/FetchMynfts/fetchBuyNfts.tsx
@@ -6,5 +6,23 @@ const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contractAddress = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 const contract = new web3.eth.Contract(NftAbi, contractAddress);
 const FetchBuyed = () => {
-  const [metaData, setMetaData] = useState([]);
+  const [nfts, setMynfts] = useState<any[]>([]);
+  const fetchNfts = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    console.log(account);
+    const myNfts = await contract.methods.fetchMyNft().call({ from: account });
+    let newData = [];
+    for (let i = 0; i < myNfts.length; i++) {
+      const tokenUri = await contract.methods
+        .tokenURI(myNfts[i].tokenId)
+        .call();
+      const metadataResponse = await fetch(ipfsToHTTPS(tokenUri));
+      if (metadataResponse.status != 200) return;
+      const json = await metadataResponse.json();
+
+      newData.push({ ...json, tokenId: myNfts[i].tokenId });
+    }
+    setMynfts(newData);
+  };
 };
