64a9bd05840d9946d913d183d62a511cbc7be4b4
Sujan Adhikari <Asujan205@gmail.com>
2023-02-05 16:00:36 +0545

    listingMyItems

--- components/FetchMynfts/fetchListed.tsx
+++ components/FetchMynfts/fetchListed.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import { NftAbi } from "../../NftAbi";
 import { ipfsToHTTPS } from "../Helper";
+import NftCard from "../MiniComponents/NftCard";
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contractAddress = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 const contract = new web3.eth.Contract(NftAbi, contractAddress);
@@ -27,4 +28,42 @@ const FetchBuyed = () => {
     }
     setMynfts(newData);
   };
+  useEffect(() => {
+    fetchNfts();
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-wrap ">
+        <p className="text-justify text-slate-200	">
+          Token I have listed for sale
+        </p>
+        {nfts.map((item, key) => {
+          return (
+            <>
+              <div className="flex-column flex-wrap space-y-2">
+                <NftCard
+                  Name={item.name}
+                  Price={item.money}
+                  description={item.description}
+                  imageUrl={item.image}
+                />
+
+                {/* <p key={key}>{item.name}</p>
+            <img
+              src={ipfsToHTTPS(item.image)}
+              alt="nfts"
+              width={45}
+              height={50}
+              key={key}
+            />
+            <p key={key}>{item.description}</p>
+            <p key={key}>{item.money}</p> */}
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
