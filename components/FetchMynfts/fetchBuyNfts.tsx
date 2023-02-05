72887d1bfb0fe1ecefd32c294c721f810991616a
Sujan Adhikari <Asujan205@gmail.com>
2023-02-05 15:44:35 +0545

    FindItems

--- components/FetchMynfts/fetchBuyNfts.tsx
+++ components/FetchMynfts/fetchBuyNfts.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import { NftAbi } from "../../NftAbi";
 import { ipfsToHTTPS } from "../Helper";
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contractAddress = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 const contract = new web3.eth.Contract(NftAbi, contractAddress);
-const FetchBuyed = () => {};
+const FetchBuyed = () => {
+  const [metaData, setMetaData] = useState([]);
+};
