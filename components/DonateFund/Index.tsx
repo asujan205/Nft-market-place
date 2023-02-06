fe8c36bf096106318780c05e38b20f1cb699d9be
Sujan Adhikari <Asujan205@gmail.com>
2023-02-06 21:51:12 +0545

    donate

--- components/DonateFund/Index.tsx
+++ components/DonateFund/Index.tsx
@@ -2,4 +2,20 @@ import React, { useState } from "react";
 
 const Donate = () => {
   const [Amount, setAmount] = useState<number>();
+  const [slider, setSlider] = useState<number>();
+
+  return (
+    <>
+      <div className="flex flex-col align-center bg-black w-40 m-20">
+        <div className="GroupInfo">
+          <p>hello</p>
+          <p>world</p>
+        </div>
+        <div className="flex flex-col">
+          <input type={"text"} className="  " />
+        </div>
+      </div>
+    </>
+  );
 };
+export default Donate;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Web3 from "web3";
 import CreationForm from "../components/CreationNft/creation";
+import Donate from "../components/DonateFund/Index";
 import OwnedNfts from "../components/FetchMynfts/Index";
 import Homepage from "../components/Homepage/Homepage";
 
 export default function Home() {
   return (
     <>
-      <OwnedNfts />
+      <Donate />
     </>
   );
 }
