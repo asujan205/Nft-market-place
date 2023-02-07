f02bc8cfd545251bddd28a3d86f4aaec0051030d
Sujan Adhikari <Asujan205@gmail.com>
2023-02-07 23:42:54 +0545

    flags

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactComponentElement } from "react";
+import React, { ReactComponentElement, useState } from "react";
 import Link from "next/link";
 type NavComponents = {
   Homepage: any;
@@ -6,36 +6,29 @@ type NavComponents = {
   mynfts: any;
 };
 
-const NavBar = (Homepage: any) => {
+const NavBar = () => {
   return (
     <>
       <nav className="bg-white border-gray-1 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col p-4 mt-4 border items-center border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link
-                href={Homepage}
+              <button
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 aria-current="page"
               >
                 Home
-              </Link>
+              </button>
             </li>
             <li>
-              <Link
-                href=""
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+              <button className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 CreateNfts
-              </Link>
+              </button>
             </li>
             <li>
-              <Link
-                href=""
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+              <button className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 Mycollections
-              </Link>
+              </button>
             </li>
           </ul>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Web3 from "web3";
 import CreationForm from "../components/CreationNft/creation";
 import Donate from "../components/DonateFund/Index";
@@ -6,9 +7,11 @@ import Homepage from "../components/Homepage/Homepage";
 import NavBar from "../components/NavBar/NavBar";
 
 export default function Home() {
+  const [Flag, setFlag] = useState<boolean>(false);
+  const [value, setValue] = useState<any>();
   return (
     <>
-      <NavBar Homepage={Homepage} />
+      <NavBar />
       <Homepage />
     </>
   );
