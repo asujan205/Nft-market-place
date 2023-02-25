7c4fee19ba227272a9f122b733d9e1b3cb569e05
asujan205 <asujan205@gmail.com>
2023-02-25 21:02:56 +0545

    swap

--- components/SwapActions/index.tsx
+++ components/SwapActions/index.tsx
@@ -4,6 +4,7 @@ import { swapabi } from "../../swapabi";
 import { FetchListedNfts } from "../Reducers/getAlldata";
 import { useDispatch } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
+import NftCard from "../MiniComponents/NftCard";
 
 const contract_address = "0x77523f004ff2eE3363853cdE2272c0f6c41A9218";
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
@@ -13,7 +14,7 @@ const Nfts_contract = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 const Createswap = () => {
   const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
 
-  const [myNfts, setMyNfts] = useState([]);
+  const [nfts, setMyNfts] = useState([]);
   const createSwap = async () => {
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
@@ -35,7 +36,34 @@ const Createswap = () => {
     <>
       <div className="flex flex-col items-center justify-center w-full h-full">
         Creating the swap
-        <button onClick={createSwap}>Create Swap</button>
+        <div className="flex flex-column gap-10 p ">
+          {nfts.map((item, key) => {
+            return (
+              <>
+                <div className="flex-column flex-wrap space-y-2 ">
+                  <NftCard
+                    Name={item.name}
+                    Price={item.money}
+                    description={item.description}
+                    imageUrl={item.image}
+                  />
+                  <button onClick={createSwap}>Create Swap</button>
+
+                  {/* <p key={key}>{item.name}</p>
+            <img
+              src={ipfsToHTTPS(item.image)}
+              alt="nfts"
+              width={45}
+              height={50}
+              key={key}
+            />
+            <p key={key}>{item.description}</p>
+            <p key={key}>{item.money}</p> */}
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
