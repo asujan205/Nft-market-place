95f7c92bb29b25e2fdd05a6b22a7fa7b998ee617
asujan205 <asujan205@gmail.com>
2023-02-25 20:57:52 +0545

    try swap

--- components/SwapActions/index.tsx
+++ components/SwapActions/index.tsx
@@ -1,15 +1,41 @@
 import Web3 from "web3";
+import { useState, useEffect } from "react";
 import { swapabi } from "../../swapabi";
+import { FetchListedNfts } from "../Reducers/getAlldata";
+import { useDispatch } from "react-redux";
+import { ThunkDispatch } from "@reduxjs/toolkit";
 
 const contract_address = "0x77523f004ff2eE3363853cdE2272c0f6c41A9218";
 const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contract = new web3.eth.Contract(swapabi, contract_address);
+const Nfts_contract = "0xBC98199BB6820dF2a57E9A417542142b6c1A46D6";
 
 const Createswap = () => {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+
+  const [myNfts, setMyNfts] = useState([]);
+  const createSwap = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    const swap = await contract.methods.createSwap().send({ from: account });
+    console.log(swap);
+  };
+
+  useEffect(() => {
+    const fetchAllNfts = async () => {
+      const newData = dispatch(FetchListedNfts());
+
+      setMyNfts((await newData).payload);
+    };
+
+    fetchAllNfts();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full h-full">
         Creating the swap
+        <button onClick={createSwap}>Create Swap</button>
       </div>
     </>
   );
