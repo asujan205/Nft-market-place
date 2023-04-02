549bb7ea96cafc65fa61fb3ae559b38321008220
asujan205 <asujan205@gmail.com>
2023-04-02 22:13:36 +0545

    send Trasactions

--- components/Wallet/MiniWallet.tsx
+++ components/Wallet/MiniWallet.tsx
@@ -11,23 +11,45 @@ const account = web3.eth.accounts.create();
 
 const address = account.address;
 
-const privateKey = account.privateKey;
-
 const web = new Web3(Web3.givenProvider || "http://localhost:7545");
 
 const MiniWallet = () => {
   const [address, setAddress] = useState<string>("");
+  const [privateKey, setPrivateKey] = useState("");
   useEffect(() => {
     const getAccount = async () => {
       const accounts = await web.eth.getAccounts();
+      let privateKey = await web.eth.accounts.privateKeyToAccount(accounts[0]);
+      setPrivateKey(privateKey);
+
       setAddress(accounts[0]);
     };
     getAccount();
   }, []);
 
-  const signTransaction = async (rawTx: any) => {
-    rawTx.gasPrice = await web.eth.getGasPrice();
-    rawTx.gasLimit = await web.eth.estimateGas(rawTx);
+  const CreateTransaction = async (rawTx: any) => {
+    rawTx.gasPrice = await web3.eth.getGasPrice();
+    rawTx.gasLimit = await web3.eth.estimateGas(rawTx);
+
+    return await web.eth.accounts.signTransaction(rawTx, privateKey);
+  };
+
+  const sendTransaction = async (signedTx: any) => {
+    return await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  };
+
+  const sendEth = async () => {
+    const rawTx = {
+      from: account.address,
+
+      value: web3.utils.toWei("0.1", "ether"),
+      gas: 21000,
+      gasPrice: 20000000000,
+    };
+
+    const signedTx = await CreateTransaction(rawTx);
+    const receipt = await sendTransaction(signedTx);
+    console.log(receipt);
   };
 };
 
