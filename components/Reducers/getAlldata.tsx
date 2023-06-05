d530abb075e45aeb7aee6aad9c4333388833f743
asujan205 <asujan205@gmail.com>
2023-06-05 14:03:14 +0545

    Rpc-change

--- components/Reducers/getAlldata.tsx
+++ components/Reducers/getAlldata.tsx
@@ -3,7 +3,9 @@ import Web3 from "web3";
 import { NftAbi } from "../../NftAbi";
 import { ipfsToHTTPS } from "../Helper";
 
-const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+// const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+const web3 = new Web3("https://rpc-mumbai.maticvigil.com");
+
 const contractAddress = "0x20445D2A57e8251ec17e9A6e111a021167fD1981";
 const contract = new web3.eth.Contract(NftAbi, contractAddress);
 
