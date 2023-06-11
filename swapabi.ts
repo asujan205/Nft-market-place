cbd3c1155b7f3b482d1f67eb9e241b1e002eaf9f
asujan205 <asujan205@gmail.com>
2023-06-11 23:27:23 +0545

    #swapabi

--- swapabi.ts
+++ swapabi.ts
@@ -1,11 +1,241 @@
 export const swapAbi: any = [
   {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "acceptSwap",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "cancelSwap",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "fee",
+        type: "uint256",
+      },
+    ],
+    name: "changeFee",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner2",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_nftContract1",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tokenId1",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_nftContract2",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tokenId2",
+        type: "uint256",
+      },
+    ],
+    name: "createSwap",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "fee",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
     anonymous: false,
     inputs: [
       {
         indexed: false,
         internalType: "uint256",
-        name: "swapId",
+        name: "fee",
+        type: "uint256",
+      },
+    ],
+    name: "FeeChange",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes",
+      },
+    ],
+    name: "onERC721Received",
+    outputs: [
+      {
+        internalType: "bytes4",
+        name: "",
+        type: "bytes4",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "rejectSwap",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner1",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner2",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "SwapCancelled",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner1",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner2",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "nftContract1",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokenId1",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "nftContract2",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokenId2",
         type: "uint256",
       },
     ],
@@ -16,33 +246,45 @@ export const swapAbi: any = [
     anonymous: false,
     inputs: [
       {
-        indexed: false,
+        indexed: true,
+        internalType: "address",
+        name: "owner1",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner2",
+        type: "address",
+      },
+      {
+        indexed: true,
         internalType: "uint256",
-        name: "swapId",
+        name: "id",
         type: "uint256",
       },
       {
-        indexed: true,
+        indexed: false,
         internalType: "address",
-        name: "from",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "tokenContract",
+        name: "nftContract1",
         type: "address",
       },
       {
         indexed: false,
         internalType: "uint256",
-        name: "tokenId",
+        name: "tokenId1",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "nftContract2",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokenId2",
         type: "uint256",
       },
     ],
@@ -50,88 +292,127 @@ export const swapAbi: any = [
     type: "event",
   },
   {
+    anonymous: false,
     inputs: [
       {
+        indexed: true,
+        internalType: "address",
+        name: "owner1",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner2",
+        type: "address",
+      },
+      {
+        indexed: true,
         internalType: "uint256",
-        name: "swapId",
+        name: "id",
         type: "uint256",
       },
     ],
-    name: "AcceptSwap",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "_swapId",
-        type: "uint256",
-      },
-    ],
-    name: "CancelSwap",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    name: "SwapRejected",
+    type: "event",
   },
   {
     inputs: [
       {
         internalType: "address",
-        name: "_to",
+        name: "newOwner",
         type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_tokenContract",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "_tokenId",
-        type: "uint256",
       },
     ],
-    name: "CreateSwap",
+    name: "transferOwnership",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
-    inputs: [
+    inputs: [],
+    name: "withdrawFees",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFee",
+    outputs: [
       {
         internalType: "uint256",
         name: "",
         type: "uint256",
       },
     ],
-    name: "_swaps",
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "getSwap",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address payable",
+            name: "owner1",
+            type: "address",
+          },
+          {
+            internalType: "address payable",
+            name: "owner2",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "nftContract1",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "tokenId1",
+            type: "uint256",
+          },
+          {
+            internalType: "address",
+            name: "nftContract2",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "tokenId2",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "isSwapped",
+            type: "bool",
+          },
+        ],
+        internalType: "struct TokenSwap.Swap",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
     outputs: [
       {
         internalType: "address",
-        name: "from",
+        name: "",
         type: "address",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenContract",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "tokenId",
-        type: "uint256",
-      },
-      {
-        internalType: "bool",
-        name: "isCompleted",
-        type: "bool",
       },
     ],
     stateMutability: "view",
